ee862d13b77e86fd58cb6c590984aecbf9e01543
kuronyago <yegor.pesterev@gmail.com>
2019-12-05 00:14:38 +0300

    gamma2

--- src/main.rs
+++ src/main.rs
@@ -15,36 +15,37 @@ fn color(r: &ray::Ray, world: &objects::Object) -> Vector {
     match hit {
         Some(record) => {
             let target: Vector = record.p + record.normal + random_in_unit_sphere();
-            let r_a: ray::Ray = ray::Ray::new(record.p, target - record.p);
-            return 0.5 * color(&r_a, world);
+            0.5 * color(&ray::Ray::new(record.p, target - record.p), world)
         }
         None => {
             let unit = r.direction.unit();
             let t = (unit.b + 1.0) * 0.5;
-            let res = (1.0 - t) * Vector::new(1.0, 1.0, 1.0) + t * Vector::new(0.5, 0.7, 1.0);
-            return res;
+            (1.0 - t) * Vector::new(1.0, 1.0, 1.0) + t * Vector::new(0.5, 0.7, 1.0)
         }
     }
 }
 
 fn random_in_unit_sphere() -> Vector {
-    let mut rng = thread_rng();
-
-    let unit = Vector::new(1.0, 1.0, 1.0);
-
+    let mut rng: ThreadRng = rand::thread_rng();
+    let mut p: Vector = Vector::new(std::f64::MAX, std::f64::MAX, std::f64::MAX);
     loop {
-        let p: Vector =
-            2.0 * Vector::new(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>()) - unit;
-
-        if p.squared_length() >= 1.0 {
-            return p;
+        if p.squared_length() < 1.0 {
+            break;
         }
+
+        p = Vector::new(
+            rng.gen_range(-1.5, 1.5),
+            rng.gen_range(-1.5, 1.5),
+            rng.gen_range(-1.5, 1.5),
+        );
     }
+    p
 }
 
 fn main() {
     let nx = 200;
     let ny = 100;
+    let nz = 100;
 
     println!("P3\n{} {}\n255", nx, ny);
 
@@ -58,18 +59,26 @@ fn main() {
         Single(Sphere::new(vector::Vector::new(0.0, -100.5, -1.0), 100.0)),
     ];
 
+    let mut rng = rand::thread_rng();
+
     let world = Multiple(objects);
 
-    let cam = Camera::new(origin, lower_left_corner, horizontal, vertical);
+    let camera = Camera::new(origin, lower_left_corner, horizontal, vertical);
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = (i as f64) / (nx as f64);
-            let v = (j as f64) / (ny as f64);
+            let mut col: Vector = Vector::new(0.0, 0.0, 0.0);
 
-            let r = cam.get_ray(u, v);
+            for _ in 0..nz {
+                let u = (f64::from(i) + rng.gen_range(0.0, 1.0)) / f64::from(nx);
+                let v = (f64::from(j) + rng.gen_range(0.0, 1.0)) / f64::from(ny);
+                let r = camera.get_ray(u, v);
+                col = col + color(&r, &world);
+            }
 
-            let col = color(&r, &world);
+            col = col / f64::from(nz);
+
+            col = Vector::new(col.a.sqrt(), col.b.sqrt(), col.c.sqrt());
 
             let ir = (255.99 * col.a) as i32;
             let ig = (255.99 * col.b) as i32;
